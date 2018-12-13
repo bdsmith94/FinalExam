37645c7ed6a38c17ddf0b00fa5aa52ede6c12c31
brenn <brenn@192.168.0.118>
2018-12-13 13:42:13 -0500

    mambo no 5

--- FinalExamProject/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExamProject/src/main/java/pkgApp/controller/RetirementController.java
@@ -6,8 +6,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import pkgApp.RetirementApp;
+import pkgCore.Retirement;
 
 public class RetirementController implements Initializable {
 
@@ -15,9 +17,35 @@ public class RetirementController implements Initializable {
 	private RetirementApp mainApp = null;
 	
 	@FXML
-	private TextField txtYearsToWork;
+    private TextField txtYearsToWork;
 	
+	@FXML
+	private Label lblSaveEachMonth;
+	
+	@FXML
+	private Label lblWhatToSaveTotal;
 
+    @FXML
+    private TextField txtAnnualReturnWorking;
+
+    @FXML
+    private TextField txtYearsRetired;
+
+    @FXML
+    private TextField txtAnnualReturnRetired;
+
+    @FXML
+    private TextField txtRequiredIncome;
+
+    @FXML
+    private TextField txtMonthlySSI;
+
+    @FXML
+    private TextField txtTotalAmountSaved;
+
+    @FXML
+    private TextField txtAmountToSave;
+    	
 	public RetirementApp getMainApp() {
 		return mainApp;
 	}
@@ -32,15 +60,42 @@ public class RetirementController implements Initializable {
 	
 	@FXML
 	public void btnClear(ActionEvent event) {
-		System.out.println("Clear pressed");
-		
-		//	TODO: Clear all the text inputs
+		//Clears the text		
+	System.out.println("Clear Pressed");
+	lblSaveEachMonth.setText("");
+	lblWhatToSaveTotal.setText("");
+	txtYearsToWork.setText("");
+	txtAnnualReturnWorking.setText("");
+	txtYearsRetired.setText("");
+	txtAnnualReturnRetired.setText("");
+	txtRequiredIncome.setText("");
+	txtMonthlySSI.setText("");
+			
 	}
 	
 	@FXML
 	public void btnCalculate(ActionEvent event) {
 		
-		//	TODO: Call AmountToSave and TotalAmountSaved and populate 
+		//	TODO: Call AmountToSave and TotalAmountSaved and populate
+		try {
+			int iYTW = Integer.parseInt(txtYearsToWork.getText());
+			int iYR = Integer.parseInt(txtYearsRetired.getText());
+			double dARW = Double.parseDouble(txtAnnualReturnWorking.getText());
+			double dARR = Double.parseDouble(txtAnnualReturnRetired.getText());
+			double dRI = Double.parseDouble(txtRequiredIncome.getText());
+			double dMSSI = Double.parseDouble(txtMonthlySSI.getText());
+		
+			Retirement R= new Retirement(iYTW,dARW,iYR,dARR,dRI,dMSSI);
+			
+			lblSaveEachMonth.setText(Double.toString(R.AmountToSave()));
+			lblWhatToSaveTotal.setText(Double.toString(R.TotalAmountSaved()));
+		}
+		
+		catch(Exception NumberformatException) {
+			System.out.println("Make sure years are in whole numbers and all required fields contain numbers.");
+			throw NumberformatException;
+		}
+		
 		
 	}
 	
--- FinalExamProject/src/main/java/pkgCore/Retirement.java
+++ FinalExamProject/src/main/java/pkgCore/Retirement.java
@@ -1,5 +1,7 @@
 package pkgCore;
 
+import org.apache.poi.ss.formula.functions.FinanceLib;
+
 public class Retirement {
 
 	private int iYearsToWork;
@@ -9,22 +11,100 @@ public class Retirement {
 	private double dRequiredIncome;
 	private double dMonthlySSI;
 	
-	//TODO: Build the contructor, getters and setters for the attributes above.
+	//Constructor for Retirement
+	public Retirement() {
+		
+	}
 	
+	public Retirement(int iY2W, double dARW, int iYR, double dARR, double dRI, double dMSSI) {
+		super();
+		this.iYearsToWork = iY2W;
+		this.dAnnualReturnWorking = dARW;
+		this.iYearsRetired = iYR;
+		this.dAnnualReturnRetired = dARR;
+		this.dRequiredIncome = dRI;
+		this.dMonthlySSI = dMSSI;
+	}
+	
+	//Setters and Getters for Retirment's Attributes
+	public int getYearsToWork() {
+		return iYearsToWork;
+	}
+	
+	public void setiYearsToWork(int iYearsToWork) {
+		this.iYearsToWork = iYearsToWork;
+	}
+	
+	public double getdAnnualReturnWorking() {
+		return dAnnualReturnWorking;
+	}
+
+	public void setdAnnualReturnWorking(double dAnnualReturnWorking) {
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+	}
+
+	public int getiYearsRetired() {
+		return iYearsRetired;
+	}
+
+	public void setiYearsRetired(int iYearsRetired) {
+		this.iYearsRetired = iYearsRetired;
+	}
+
+	public double getdAnnualReturnRetired() {
+		return dAnnualReturnRetired;
+	}
+
+	public void setdAnnualReturnRetired(double dAnnualReturnRetired) {
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+	}
+
+	public double getdRequiredIncome() {
+		return dRequiredIncome;
+	}
+
+	public void setdRequiredIncome(double dRequiredIncome) {
+		this.dRequiredIncome = dRequiredIncome;
+	}
+
+	public double getdMonthlySSI() {
+		return dMonthlySSI;
+	}
+
+	public void setdMonthlySSI(double dMonthlySSI) {
+		this.dMonthlySSI = dMonthlySSI;
+	}
+	
+	//AmountToSave Determines the amount to save each month based on TotalAmountSaved, YearsToWork, 
+	//and Annual Return While Working.
 	public double AmountToSave()
 	{
-		//TODO: Determine the amount to save each month based on TotalAmountSaved, YearsToWork
-		//		and Annual return while working
+		double monthlyReturn = dAnnualReturnWorking / 1200;
+		int monthsToWork = iYearsToWork * 12;
+		int amount = 0;
+		double TotalAmountSaved = TotalAmountSaved();
+		boolean tf = false;
 		
+		double pmt = FinanceLib.pmt(monthlyReturn, monthsToWork, amount, TotalAmountSaved, tf);
 		
-		return 0;
+		return pmt*(-1); 
 	}
 	
 	public double TotalAmountSaved()
 	{
-		//	TODO: Determine amount to be saved based on Monthly SSI, Required Income, Annual return during retirement
-		//		and number of years retired.
-		//
-		return 0;
+		//TotalAmountSave determines the TotalAmountSaved based on Montly SSI, Required Income,
+		//Annual Return during Retirment, and number of years retired
+		
+		double monthlyReturn = dAnnualReturnRetired / 1200;
+		double monthsRetired = iYearsRetired * 12;
+		double amountEarned = dRequiredIncome - dMonthlySSI;
+		int amount = 0;
+		boolean tf = false;
+		
+		double pv = FinanceLib.pv(monthlyReturn, monthsRetired, amountEarned, amount, tf);
+		
+		return pv*(-1);
+		
+		
 	}
 }
